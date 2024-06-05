b037680555c727d8530d7aea788422efec55c9fb
Nixon Deb Antu <nixondebantu@gmail.com>
2024-06-05 12:15:40 +0600

    connected login api with toast

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/toaster";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -27,6 +28,7 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+        <Toaster />
       </body>
     </html>
   );
--- client/components/signinpage/SignInCard.tsx
+++ client/components/signinpage/SignInCard.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -10,9 +11,55 @@ import {
 import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useToast } from "@/components/ui/use-toast";
+import { BACKENDURL } from "@/data/urls";
+import axios from "axios";
 import Link from "next/link";
+import { useState } from "react";
 
 export function SignInCard() {
+  const { toast } = useToast();
+  const [regno, setRegno] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [longsession, setLongsession] = useState<boolean>(false);
+
+  const handleSubmit = async () => {
+    if (regno === "" || password === "") {
+      toast({
+        variant: "destructive",
+        title: "Credential missing",
+        description: "Enter registration number and password to login",
+        duration: 3000,
+      });
+    } else {
+      try {
+        const response = await axios.post(`${BACKENDURL}auth/login`, {
+          regno,
+          password,
+          longsession,
+        });
+        if (response.status === 200) {
+          toast({
+            title: "Logged in successfully",
+            description: `${response.data.user.fullname} welcome to SWE Society`,
+          });
+          // redirect to "/dashboard/profile"
+        }
+      } catch (error: any) {
+        if (error.response.status === 404) {
+          toast({
+            title: error.response.data.message,
+          });
+        } else if (error.response.status === 500) {
+          toast({
+            title: error.response.data.message,
+            description: error.response.data.details,
+          });
+        }
+      }
+    }
+  };
+
   return (
     <Card className="w-full max-w-sm">
       <CardHeader>
@@ -24,14 +71,31 @@ export function SignInCard() {
       <CardContent className="grid gap-4">
         <div className="grid gap-2">
           <Label htmlFor="number">Registration Number</Label>
-          <Input id="number" type="number" placeholder="2020831000" required />
+          <Input
+            id="number"
+            type="number"
+            placeholder="2020831000"
+            value={regno}
+            onChange={(e) => setRegno(e.target.value)}
+            required
+          />
         </div>
         <div className="grid gap-2">
           <Label htmlFor="password">Password</Label>
-          <Input id="password" type="password" required />
+          <Input
+            id="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
         </div>
         <div className="flex justify-items-start items-center gap-2">
-          <Checkbox id="remember" />
+          <Checkbox
+            id="remember"
+            checked={longsession}
+            onClick={(e) => setLongsession(!longsession)}
+          />
           <label
             htmlFor="remember"
             className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
@@ -41,7 +105,9 @@ export function SignInCard() {
         </div>
       </CardContent>
       <CardFooter className="flex flex-col ">
-        <Button className="w-full">Sign in</Button>
+        <Button type="submit" className="w-full" onClick={handleSubmit}>
+          Sign in
+        </Button>
         <Link
           href="/forgetpass"
           className="ml-auto py-2 inline-block text-sm underline"
