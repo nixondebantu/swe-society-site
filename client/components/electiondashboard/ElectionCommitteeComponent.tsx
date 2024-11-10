f75440adc82939ed2435895c9edc2c6cd3382973
Nafi Ullah Shafin <shafinnafiullah@gmail.com>
2024-11-10 18:16:32 +0600

    added election edit

--- client/components/electiondashboard/ElectionCommitteeComponent.tsx
+++ client/components/electiondashboard/ElectionCommitteeComponent.tsx
@@ -1,5 +1,7 @@
 "use client"
-import React from "react";
+import React, { useState } from "react";
+import { MdDelete, MdModeEditOutline } from "react-icons/md";
+import ElectionEditModal from "./ElectionEditModal";
 
 interface ElectionCommitteeProps {
   electionCommittees: {
@@ -22,8 +24,26 @@ interface ElectionCommitteeProps {
   setSelectedElectionId: React.Dispatch<React.SetStateAction<number | null>>;
 }
 
-const ElectionCommitteeComponent: React.FC<ElectionCommitteeProps> = ({ electionCommittees, setShowFullCommitteee, setSelectedElectionId }) => {
+interface ElectionFormData {
+    year: string;
+    election_type: string;
+    candidate_form_date: string;
+    election_date: string;
+    election_commissioner?: number;
+    assistant_commissioner?: number;
+  }
 
+const ElectionCommitteeComponent: React.FC<ElectionCommitteeProps> = ({ electionCommittees, setShowFullCommitteee, setSelectedElectionId }) => {
+    const [openEditModal, setOpenEditModal] = useState(false);
+    const [editingElectionData, setEditingElectionData]= useState<ElectionFormData>({
+        year: "",
+        election_type: "",
+        candidate_form_date: "",
+        election_date: "",
+        election_commissioner: 1,
+        assistant_commissioner: 1
+    });
+    const [editElectionId, setEditElectionId] = useState<number>(0);
     const formatDate = (dateString: string | null) => {
         if (!dateString) return "N/A";
         const date = new Date(dateString);
@@ -31,6 +51,7 @@ const ElectionCommitteeComponent: React.FC<ElectionCommitteeProps> = ({ election
       };
 
   return (
+    <>
     <div className="w-full p-4 grid gap-4 md:grid-cols-2 lg:grid-cols-3">
       {electionCommittees.map((election) => (
         <div
@@ -41,9 +62,34 @@ const ElectionCommitteeComponent: React.FC<ElectionCommitteeProps> = ({ election
           key={election.electionid}
           className="bg-gray-900 hover:bg-black cursor-pointer shadow-md rounded-lg p-6 flex flex-col space-y-4 border border-gray-700"
         >
-  
+                
           <div className="space-y-2">
+            <div className="flex justify-between">
             <h4 className="text-lg font-bold text-red-300">Election Details</h4>
+                <div className="flex space-x-3">
+                    <button 
+                     onClick={(event) => {
+                        event.stopPropagation(); 
+                        setEditingElectionData({
+                            year: election.year || "",
+                            election_type: election.election_type || "",
+                            candidate_form_date: election.candidate_form_date || "",
+                            election_date: election.election_date || "",
+                            election_commissioner: election.commissioner_userid || 0 ,
+    assistant_commissioner: election.assistant_userid || 0,
+                        });
+                        setEditElectionId(election.electionid)
+                        setOpenEditModal(true);
+                      }}
+                    className="p-2 rounded border border-red-300  flex items-center justify-center">
+                        <MdModeEditOutline className="text-red-300 text-sm"/>
+                    </button>
+                    <button className="p-2 rounded border border-red-300  flex items-center justify-center">
+                        <MdDelete className="text-red-300 text-sm"/>
+                    </button>
+                </div>
+            </div>
+           
             <p className="font-bold"> {election.election_type || ""} Election {election.year || ""}</p> 
            
             
@@ -87,9 +133,18 @@ const ElectionCommitteeComponent: React.FC<ElectionCommitteeProps> = ({ election
             </div>
           )}
 
+
+
         </div>
       ))}
+
+
+     
     </div>
+    {openEditModal && (
+         <ElectionEditModal onClose={() => setOpenEditModal(false)}  prevformData={editingElectionData} electionId={editElectionId}/>
+      )}
+    </>
   );
 };
 
