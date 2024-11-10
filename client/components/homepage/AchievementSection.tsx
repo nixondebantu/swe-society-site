e2b636fd2e7795ac1ebf1eb33dcd0d1d64fbb324
Arnob-sen <arnobsen16.2@gmail.com>
2024-11-10 21:51:48 +0600

    add_button

--- client/components/homepage/AchievementSection.tsx
+++ client/components/homepage/AchievementSection.tsx
@@ -1,5 +1,7 @@
 import Achievement from "@/app/dashboard/[menuitem]/_pages/Achievement";
 import { HomeAchievementCard } from "../achievementspage/homeAchievementCard";
+import Link from "next/link";
+import { Button } from "../ui/button";
 
 function AchievementSection() {
   return (
@@ -7,7 +9,10 @@ function AchievementSection() {
       {/* <h1 className="flex flex-col items-center">Achievements</h1> */}
       <h1 className="flex flex-col items-center font-bold text-primary">Achievements</h1>
       <HomeAchievementCard />
-      
+      <Link href={'/achievements'}>
+      <div className='flex flex-col items-end justify-end p-10'>    
+          <Button className='flex flex-col justify-center items-end'>All Achievements</Button></div>
+          </Link>
        <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
     </div>
   );
--- client/components/homepage/EventSection.tsx
+++ client/components/homepage/EventSection.tsx
@@ -1,8 +1,12 @@
 
+'use client'
 import { Suspense } from 'react';
 import { EventMarqueeDemo } from "../eventspage/EventCard";
-
+import { Button } from '../ui/button';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 function EventSection() {
+  const path = usePathname();
   return (
     <div>
       {/* <h1 className="flex flex-col justify-center items-center">Events</h1> */}
@@ -11,7 +15,15 @@ function EventSection() {
      
         <EventMarqueeDemo />
       </Suspense>
+      {path==='/'&&(
+      <Link href={'/events'}>
+      <div className='flex flex-col items-end justify-end p-10'>    
+          <Button className='flex flex-col justify-center items-end'>All Event</Button></div>
+          </Link>
+          )}
       <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
+      
+
     </div>
   );
 }
