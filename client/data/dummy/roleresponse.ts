7a52be901401845ca147d2cb4edd8b6ef72385d9
Nixon Deb Antu <nixondebantu@gmail.com>
2024-11-22 23:56:56 +0600

    dynamic role based dashboard menu

--- client/data/dummy/roleresponse.ts
+++ client/data/dummy/roleresponse.ts
@@ -1,16 +1,26 @@
-import { RoleAccessType } from "../types";
+import { RoleAccessType } from "@/data/types";
+import axios from "axios";
+import { APIENDPOINTS } from "../urls";
 
-export const roleAccess: RoleAccessType = {
-  statistics: true,
-  achievement: true,
-  achievementmanage: true,
-  blog: true,
-  member: true,
-  notice: true,
-  bulkmail: true,
-  landingpage: true,
-  events: true,
-  ec: true,
-  roles: true,
-  usersblog: true,
+export const fetchRoleAccess = async (): Promise<RoleAccessType> => {
+  try {
+    const response = await axios.get(APIENDPOINTS.users.getRoleAccess);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching role access:", error);
+    return {
+      statistics: false,
+      achievement: false,
+      achievementmanage: false,
+      blog: false,
+      usersblog: false,
+      member: false,
+      notice: false,
+      bulkmail: false,
+      landingpage: false,
+      events: false,
+      ec: false,
+      roles: false,
+    };
+  }
 };
