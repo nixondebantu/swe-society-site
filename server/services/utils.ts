1294aa41833823c3195b1e106512a836a9cfc109
nixondebantu <nixondebantu@gmail.com>
2024-11-24 14:29:43 +0600

    create backend for OTP and password reset

--- server/services/utils.ts
+++ server/services/utils.ts
@@ -1,14 +1,17 @@
 function generateRandomPassword(n: number): string {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    let password = '';
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let password = "";
 
-    for (let i = 0; i < n; i++) {
-        const randomIndex = Math.floor(Math.random() * characters.length);
-        password += characters[randomIndex];
-    }
+  for (let i = 0; i < n; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    password += characters[randomIndex];
+  }
 
-    return password;
+  return password;
 }
 
+const generateOTP = (): string => {
+  return Math.floor(100000 + Math.random() * 900000).toString();
+};
 
-export { generateRandomPassword };
+export { generateRandomPassword, generateOTP };
